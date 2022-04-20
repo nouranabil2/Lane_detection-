cc8e74751c8b93bd144cebd664d75f9340fbdedc
noura <1701595@eng.asu.edu.eg>
2022-04-20 08:54:39 +0200

    importing threshold and perspective file

--- saving.py
+++ saving.py
@@ -6,121 +6,7 @@ import cv2
 from IPython.core.display import Video
 import glob
 from moviepy.editor import *
-
-def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
-
-    # Apply threshold
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # Apply x or y gradient with the OpenCV Sobel() function
-    # and take the absolute value
-    if orient == 'x':
-        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
-    if orient == 'y':
-        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
-    # Rescale back to 8 bit integer
-    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
-    # Create a copy and apply the threshold
-    binary_output = np.zeros_like(scaled_sobel)
-    # Here I'm using inclusive (>=, <=) thresholds, but exclusive is ok too
-    binary_output[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
-
-    # Return the result
-    return binary_output# retuen  x or y
-
-
-def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
-
-    # Apply threshold
-    # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # Take both Sobel x and y gradients
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-    # Calculate the gradient magnitude
-    gradmag = np.sqrt(sobelx**2 + sobely**2)
-    # Rescale to 8 bit
-    scale_factor = np.max(gradmag)/255 
-    gradmag = (gradmag/scale_factor).astype(np.uint8) 
-    # Create a binary image of ones where threshold is met, zeros otherwise
-    binary_output = np.zeros_like(gradmag)
-    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
-
-    # Return the binary image
-    return binary_output
-
-def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
-
-    # Apply threshold
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # Calculate the x and y gradients
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-    # Take the absolute value of the gradient direction, 
-    # apply a threshold, and create a binary image result
-    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
-    binary_output =  np.zeros_like(absgraddir)
-    binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
-
-    # Return the binary image
-    return binary_output
-def combined_thresholds(image, ksize = 3):
-    # Choose a Sobel kernel size
-
-    # Apply each of the thresholding functions
-    gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(5, 100))
-    grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(5, 100))
-    #c= cv2.bitwise_or(gradx,grady)
-    
-    mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(3, 255))
-    dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0*np.pi/180, 90*np.pi/180)) 
-    combined = np.zeros_like(dir_binary, np.uint8)    
-    #combined[((gradx == 1) ) ] = 1
-    combined[((gradx == 1) ) & ((mag_binary == 1) & (dir_binary == 1))] = 1
-    #combined[ (mag_binary == 1) ] = 1
-
-    return combined
-
-def filter(image):
-    
-    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
-    s_channel = hls[:,:,2]
-    l_channel = hls[:,:,1]
-
-    sxbinary = combined_thresholds(image)
-
-
-    # Threshold color channel
-    s_thresh_min = 40
-    s_thresh_max = 255
-    l_thresh_min = 150
-    l_thresh_max = 255  
-    
-    
-    s_binary = np.zeros_like(s_channel)
-    s_binary[((s_channel >= s_thresh_min) & (s_channel <= s_thresh_max)) ] = 1
-    
-    l_binary = np.zeros_like(s_channel)
-    l_binary[((l_channel >= l_thresh_min) & (l_channel <= l_thresh_max))] = 1
-
-    # Combine the two binary thresholds
-    combined_binary = np.zeros_like(sxbinary)
-    #combined_binary [(l_binary == 1)& (sxbinary == 1)] = 1
-    combined_binary[((s_binary == 1) & (sxbinary == 1)) | ((sxbinary == 1) & (l_binary == 1))] = 1
-    
-    return combined_binary
-
-
-def warp(image,state='in'):
-
-    src = np.float32([[570,460],[image.shape[1] - 573,460],[image.shape[1] - 150,image.shape[0]],[150,image.shape[0]]])
-    dst = np.float32([[200,0],[image.shape[1]-200,0],[image.shape[1]-200,image.shape[0]],[200,image.shape[0]]])
-    if state == 'in':
-        M = cv2.getPerspectiveTransform(src, dst)
-    if state == 'out':
-        M = cv2.getPerspectiveTransform(dst, src)
-    
-    warped = cv2.warpPerspective(image, M, (image.shape[1],image.shape[0]), flags=cv2.INTER_LINEAR)
-    return warped
+from threshold import *
 
 class LaneDetector:
     
@@ -367,7 +253,7 @@ def combine():
     second_half=clips_array([[second_half1,second_half3]])
     video=clips_array([[first_half.set_position(600,400)],[second_half.set_position(600,400)]])
     video.ipython_display(width=1200,height=700)
-    video.write_videofile('Total.mp4')
+    video.write_videofile(strout)
     return print('Done')
 
 def covert_filter(image):
@@ -403,4 +289,4 @@ def saving(strin,strout,mode):
 
 strin="challenge_video.mp4"
 strout='tttt_out.mp4'
-saving(strin,strout,0)
+saving(strin,strout,1)
